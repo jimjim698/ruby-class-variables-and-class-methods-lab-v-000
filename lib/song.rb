d7be27341f2eeb3fde0648a6a6ab73e8fefad166
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-26 19:46:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,14 @@
 class Song
   attr_accessor :name, :genre, :artist
   @@count = 0
+  @@genres =[]
 
 def  initialize(name,artist, genre)
   @name = name
   @artist = artist
   @genre = genre
   @@count +=1
+  @@genres<< genre
 end
 
   def self.count
